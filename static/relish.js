b451aa3d8dd2607b6cc6cb35fcc711a1dab57410
angelachan64 <angelachan64@gmail.com>
2015-12-13 22:57:11 +0000

    some more progress

--- static/relish.js
+++ static/relish.js
@@ -3,8 +3,23 @@ var start = function(){
     if(count%2==0){
         console.log("started");
         $(".btn").html('STOP');
+        $("#timer").html("99%");
+        $("#timer").css({
+        'width':'99%'
+        });
+        var i = 1;
+        while (i < 26) {
+            $("#"+i.toString()).fadeOut();
+            i++;
+        }
+        
     } else{
         console.log("stopped");
         $(".btn").html("START");
+        $("#timer").html("100%");
+        $("#timer").css({
+        'width':'100%'
+        });
     } count++;
 };
+
