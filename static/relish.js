7b51140fef87ad73bcd71a169c78d7cf24ffe37f
angelachan64 <angelachan64@gmail.com>
2015-12-14 00:56:45 +0000

    works??

--- static/relish.js
+++ static/relish.js
@@ -1,25 +1,86 @@
 var count = 0;
+var time = 100;
+var score = 0;
+var myVar;
+var mole;
 var start = function(){
     if(count%2==0){
         console.log("started");
         $(".btn").html('STOP');
-        $("#timer").html("99%");
-        $("#timer").css({
-        'width':'99%'
-        });
-        var i = 1;
-        while (i < 26) {
-            $("#"+i.toString()).fadeOut();
-            i++;
-        }
+        startTimer();
+        clear();
+        play();
         
     } else{
         console.log("stopped");
         $(".btn").html("START");
-        $("#timer").html("100%");
-        $("#timer").css({
-        'width':'100%'
+        stopTimer();
+        clearInterval(mole);
+        score = 0;
+        $("#completion").html(score.toString()+"%");
+        $("#completion").css({
+            'width':'0%'
         });
     } count++;
 };
 
+function play() {
+    console.log("playing");
+    mole = setInterval(function(){
+        var id = (Math.floor(Math.random()*(25))+1).toString();
+        var id2 = (Math.floor(Math.random()*(25))+1).toString();
+        while (id2 == id){
+            id2 = (Math.floor(Math.random()*(25))+1).toString();
+        }
+        console.log(id);
+        console.log(id2);
+        $("#"+id).fadeIn();
+        setTimeout(function(){
+            $("#"+id).fadeOut();
+        }, 1500);
+        $("#"+id2).fadeIn();
+        setTimeout(function(){
+            $("#"+id2).fadeOut();
+        }, 1500);
+    }, 2000);
+}
+function clear() {
+    var i = 1;
+    while (i < 26) {
+        $("#"+i.toString()).fadeOut();
+        i++;
+    }
+}
+
+var scoreUp = function(e) {
+    score++;
+    console.log((score*10).toString());
+    $("#completion").html((score*10).toString()+"%");
+    $("#completion").css({'width':((score*10).toString()+"%")});
+    if((score*10) >= 100){
+        console.log("win!");
+        clearInterval(mole);
+        clearInterval(myVar);
+        $("#completion").html("100%");
+        $("#completion").css({'width':"100%"});
+    }
+};
+
+var startTimer = function(){
+    myVar = setInterval(function(){
+        time--;
+        $("#timer").html(time.toString()+"%");
+        $("#timer").css({
+           'width':time.toString()+'%' 
+        });
+    }, 1000);
+};
+
+var stopTimer = function(){
+    clearInterval(myVar);
+    time = 100;
+    $("#timer").html(time.toString()+"%");
+    $("#timer").css({
+        'width':time.toString()+'%'
+    });
+};
